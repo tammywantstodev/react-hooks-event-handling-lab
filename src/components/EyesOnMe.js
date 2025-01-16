0683f305c0c4508803b4dc104864e5e18587a12f
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-01-16 14:09:53 +0300

    Saving changes

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return (
+        <div>
+            <button onBlur={(e) => {
+                console.log('Hey! Eyes on me!');
+            }}
+                    onFocus={(e) => {
+                        console.log('Good!');
+                    }}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
